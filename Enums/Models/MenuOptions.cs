c6e3c0e0898049692c73057ffbdb656784aa1948
TildaDares <mathildaudufo@gmail.com>
2024-10-24 15:39:56 +0100

    add enum for menuoptions

---
+++ Enums/Models/MenuOptions.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel;
+using System.Runtime.Serialization;
+
+namespace CodingTracker.Enums;
+
+public enum MenuOptions
+{
+    [Description("Insert coding session")]
+    InsertCodingSession = 0,
+    [Description("Get coding session")]
+    GetCodingSession,
+    [Description("Get coding sessions")]
+    GetCodingSessions,
+    [Description("Update coding session")]
+    UpdateCodingSession,
+    [Description("Delete coding session")]
+    DeleteCodingSession,
+    [Description("Exit")]
+    Exit
+}
