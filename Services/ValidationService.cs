d57c2130a1e928df024e596375b29f3699cc23e3
TildaDares <mathildaudufo@gmail.com>
2024-10-24 16:15:45 +0100

    CHEck if date is in range

--- Services/ValidationService.cs
+++ Services/ValidationService.cs
@@ -4,11 +4,19 @@ namespace CodingTracker;
 
 public static class ValidationService
 {
-    public static Validator IsDateValid(string dateString)
+    public static Validator IsDateValid(string dateString, DateTime? minRange = null, DateTime? maxRange = null)
     {
+        minRange ??= DateTime.MinValue;
+        maxRange ??= DateTime.MaxValue;
         var enUS = new CultureInfo("en-US");
-        var dateParse = DateTime.TryParseExact(dateString, "dd/MM/yyyy HH:mm", enUS,
+        var isValid = DateTime.TryParseExact(dateString, "dd/MM/yyyy HH:mm", enUS,
             DateTimeStyles.None, out var dateValue);
-        return dateParse ? new Validator(true, dateTime: dateValue) : new Validator(false, "Invalid date format!", dateTime: dateValue);
+        var isWithinRange = dateValue >= minRange && dateValue <= maxRange;
+
+        var message = "";
+        if (!isValid) message = "Invalid format";
+        else if (!isWithinRange) message = "Date is out of range!";
+        
+        return new Validator(isValid && isWithinRange, message, dateTime: dateValue);
     }
 }
